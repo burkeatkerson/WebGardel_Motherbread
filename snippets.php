7eb73350beafb1553a4a6245dc8e1e253d6566b3
Burke Atkerson <burkeatkerson@gmail.com>
2014-06-27 19:48:16 -0300

    updated snippets
    Includes the wg_SDK.php inclusions.
    
    Needs a standardized SDK file.
    Needs a motherbread using new wg_SDK system.

--- snippets.php
+++ snippets.php
@@ -3,9 +3,19 @@
 <!-- Paste at the very top of the index page -->
 <?php
     include_once('sbwebsite.php');
-    $site = new SBWebsite();
-	
+    $site = new SBWebsite();	
 ?>
+
+
+<!-- Paste in the head section over the Title tag -->
+    <title><?= $site->data('company_name') ?></title>
+    <? //Meta tags for Facebook, sharing, and SEO ?>
+    <meta property="og:title" content="<?= $site->data('company_name') ?> <?= $site->data('slogan_text') ?>"/>
+	<meta property="og:image" content="http://graph.facebook.com/<? echo $site->data('fb_id') ?>/picture?type=large"/>
+	<meta property="og:site_name" content="<?= $site->data('domain_name') ?>"/>
+	<meta property="og:url" content="http://<?= $site->data('domain_name') ?>"/>
+	<meta property="og:description" content="<? echo $site->content('long_description') ?>"/>
+    <meta name="description" content="<?= $site->data('short_description') ?>">
 
 <!-- Paste after the closed head tag before body tag fpr fbFeed plugin -->
 <?php $fbfeed_path = 'fbfeed';
@@ -28,49 +38,8 @@ $('#cff .cff-item').each(function(){
   $cffphoto.detach();
   $self.find('.cff-post-text').before($cffphoto);
 });</script>
-<? $fb_likebutton = '<div class="fb-like" data-href="'.$site->data('fb_page_url').'"  data-layout="button" data-action="like" data-show-faces="true" data-share="false"></div>'; 
-$fb_followbox_standard = '<div class="fb-follow" data-href="'.$site->data('fb_page_url').'" data-width="250" data-height="250" data-colorscheme="light" data-layout="standard" data-show-faces="true"></div>';
-$fb_followbutton = '<div class="fb-follow" data-href="'.$site->data('fb_page_url').'" data-width="250" data-height="250" data-colorscheme="light" data-layout="button_count" data-show-faces="true"></div>';
-$fb_sharebutton_box_small = '<div class="fb-share-button" data-href="'.$site->data('fb_page_url').'" data-type="box_count"></div>';
-$fb_sharebutton = '<div class="fb-share-button" data-href="'.$site->data('fb_page_url').'" data-type="button"></div>'; 
-$fb_sharebutton_horiz = '<div class="fb-share-button" data-href="'.$site->data('fb_page_url').'" data-type="button_count"></div>';
-$fb_comments = '<div class="fb-comments" data-href="http://www.facebook.com/webgardel" data-width="590" data-numposts="12" data-colorscheme="light"></div>';
-$fb_likebox = '<div class="fb-like-box" data-href="'.$site->data('fb_page_url').'" data-width="300" data-height="600" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>';
-$gmaps_iframe = '<iframe src="'.$site->data('gmaps_embed_url').'" width="100%" height="100%" frameborder="0" style="border:0;"></iframe>';
-$menu_li_1 = 'inicio';
-$menu_li_2 = 'nosotros';
-$menu_li_3 = 'contacto';
-$menu_li_4 = 'fotos';
-$menu_li_5 = 'novedades';
-$menu_li_6 = 'más info';
-$menu_li_7 = 'dirección';
-$menu_li_8 = 'página de facebook';
+<? 
 
-if ($menu_li_1 != 'inicio') {
-	$menu_item1 = '<a href="#'.$menu_li_1.'">'.$menu_li_1.'</a>';
-	}
-	else {
-	$menu_item1 = '<a href="./">'.$menu_li_1.'</a>';
-	}
-$menu_item2 = '<a href="#'.$menu_li_2.'">'.$menu_li_2.'</a>';
-$menu_item3 = '<a href="#'.$menu_li_3.'">'.$menu_li_3.'</a>';
-$menu_item4 = '<a href="#'.$menu_li_4.'">'.$menu_li_4.'</a>';
-$menu_item5 = '<a href="#'.$menu_li_5.'">'.$menu_li_5.'</a>';
-$menu_item6 = '<a href="#'.$menu_li_6.'">'.$menu_li_6.'</a>';
-$menu_item7 = '<a href="#'.$menu_li_7.'">'.$menu_li_7.'</a>';
-$menu_item8 = '<a href="'.$site->data('fb_page_url').'" target="_blank">'.$menu_li_8.'</a>';
-
-$fb_prof_pic = '<img src="http://graph.facebook.com/'. $site->data('fb_id') .'/picture?type=large" style="max-width:150px; max-height:120px; border-radius:15px;"/>';
-$fb_prof_pic_small = '<img src="http://graph.facebook.com/'. $site->data('fb_id') .'/picture" style="border-radius:5px; float:left; margin:5px 15px 5px 0;"/>';
-
-$albumid = '281475991951856'; //if want a specific album
-$contents = file_get_contents('http://graph.facebook.com/'.$site->data('fb_id').'/photos/uploaded?limit=12');
-$photos = json_decode($contents,true);
-$photos = $photos['data'];
-
-$get_fb_photo_cover = file_get_contents('https://graph.facebook.com/'.$site->data('fb_id').'?fields=cover');
-$decode_photo_cover = json_decode($get_fb_photo_cover);
-$cover_image = $decode_photo_cover->cover ->source;
 $fb_cover_photo = '<div style="max-height:500px; overflow:hidden;"><img class="rsImg" src="'.$cover_image.'" alt="'.$site->data('company_name').'" /></div>';
 
 
@@ -96,17 +65,19 @@ $fb_cover_photo = '<div style="max-height:500px; overflow:hidden;"><img class="r
 
 
 <!-- Copy and paste specifically where you want the FACEBOOK WIDGET to show up -->
-<? echo $fb_prof_pic ?>
-<? echo $fb_prof_pic_small ?>
-<? echo $fb_cover_photo ?>
-<? echo $fb_likebutton; ?>
-<? echo $fb_followbox_standard; ?>
-<? echo $fb_followbutton; ?>
-<? echo $fb_sharebutton_box_small; ?>
-<? echo $fb_sharebutton; ?>
-<? echo $fb_sharebutton_horiz; ?>
-<? echo $fb_comments; ?>
-<? echo $fb_likebox; ?>
+<? echo $fb_widget['likebutton']; ?>
+<? echo $fb_widget['followbox_standard']; ?>
+<? echo $fb_widget['followbutton']; ?>
+<? echo $fb_widget['sharebutton_box_small']; ?>
+<? echo $fb_widget['sharebutton']; ?>
+<? echo $fb_widget['sharebutton_horiz']; ?>
+<? echo $fb_widget['comments']; ?>
+<? echo $fb_widget['likebox']; ?>
+<? echo $fb_widget['likebox_tall']; ?>
+<? echo $fb_widget['prof_pic']; ?>
+<? echo $fb_widget['prof_pic_small']; ?>
+<? echo $fb_widget['cover_photo']; ?>
+
 <? echo $fb_timeline; //facebook feed, nodevades, MURO, using socoaltimeline Plugin ?>
 <? fbFeed($settings); //facebook feed, nodevades, MURO, timeline using fbFeedPlugin ?>  
 
